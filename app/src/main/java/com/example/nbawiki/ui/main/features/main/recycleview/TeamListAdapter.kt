9493d1a8dd7e2fedcc1de0aa98b88c686e044dbf
Augustas dirzys <spekulents@gmail.com>
2020-01-09 12:26:29 +0200

    implement basic navigation  on team card click, the bad way

--- app/src/main/java/com/example/nbawiki/ui/main/features/main/recycleview/TeamListAdapter.kt
+++ app/src/main/java/com/example/nbawiki/ui/main/features/main/recycleview/TeamListAdapter.kt
@@ -1,13 +1,15 @@
 package com.example.nbawiki.ui.main.features.main.recycleview
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.example.nbawiki.R
 import com.example.nbawiki.model.Team
 
 
-class TeamListAdapter(private val list: List<Team>)
-    : RecyclerView.Adapter<TeamViewHolder>() {
+class TeamListAdapter(private val list: List<Team>) : RecyclerView.Adapter<TeamViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TeamViewHolder {
         val inflater = LayoutInflater.from(parent.context)
@@ -20,6 +22,10 @@ class TeamListAdapter(private val list: List<Team>)
     override fun onBindViewHolder(holder: TeamViewHolder, position: Int) {
         val team: Team = list[position]
         holder.bind(team)
+        holder.itemView.setOnClickListener {
+            Log.e("TeamListAdapter", "${team.teamName}")
+            it.findNavController().navigate(R.id.action_mainFragment_to_teamFragment)
+        }
     }
 
     override fun getItemCount(): Int = list.size
--- app/src/main/java/com/example/nbawiki/ui/main/features/main/recycleview/TeamViewHolder.kt
+++ app/src/main/java/com/example/nbawiki/ui/main/features/main/recycleview/TeamViewHolder.kt
@@ -1,8 +1,11 @@
 package com.example.nbawiki.ui.main.features.main.recycleview
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.nbawiki.R
 import com.example.nbawiki.model.Team
@@ -15,11 +18,20 @@ class TeamViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
     init {
         mTitleView = itemView.findViewById(R.id.team_title)
         mDescriptionView = itemView.findViewById(R.id.team_description)
+        val card : CardView =itemView.findViewById(R.id.team_card)
+
+//        card.setOnClickListener {
+//            Log.e("TeamViewHolder","${mDescriptionView.text)}")
+//            parent.findNavController().navigate(R.id.action_mainFragment_to_teamFragment)
+//        }
+
     }
 
     fun bind(team: Team) {
         mTitleView?.text = team.teamName
         mDescriptionView?.text = team.teamDescription
     }
+
+
 
 }
