4e4c0585de693f79e67ede1f20dda822ccfa40c6
Augustas dirzys <spekulents@gmail.com>
2020-01-09 15:00:50 +0200

    On Creation team fragment receives team name from main fragment

--- app/src/main/java/com/example/nbawiki/ui/main/team/TeamFragment.kt
+++ app/src/main/java/com/example/nbawiki/ui/main/team/TeamFragment.kt
@@ -1,15 +1,16 @@
 package com.example.nbawiki.ui.main.team
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.example.nbawiki.R
 
 class TeamFragment : Fragment() {
+
+    lateinit var teamName : String
 
     companion object {
         fun newInstance() = TeamFragment()
@@ -21,8 +22,15 @@ class TeamFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        arguments?.let {
+           teamName = TeamFragmentArgs.fromBundle(it).teamName
+        }
+
         return inflater.inflate(R.layout.team_fragment, container, false)
+
     }
+
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
