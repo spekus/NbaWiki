17497d6f711b3b5b3266c26c3de8522ab91768b7
Augustas dirzys <spekulents@gmail.com>
2020-01-10 16:18:51 +0200

    Change defoult player photo, add description field to player data model

--- app/src/main/java/com/example/nbawiki/model/Player.kt
+++ app/src/main/java/com/example/nbawiki/model/Player.kt
@@ -1,10 +1,13 @@
 package com.example.nbawiki.model
 
+import java.util.*
+
 data class Player (
     val id : Int = 0,
     val name : String = "defaulName",
     val sureName : String = "defaulSureName",
     val age : Int = (20..50).random(),
     val height : Int = (160..250).random(),
-    val weight : Int = (80..150).random()
+    val weight : Int = (80..150).random(),
+    val description : String = UUID.randomUUID().toString()
 )
