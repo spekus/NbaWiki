9a62d182786d57a1d633763b7d78f0e36db0f467
Augustas dirzys <spekulents@gmail.com>
2020-01-10 10:28:42 +0200

    Add two different fragments for 2 tabs

--- app/src/main/java/com/example/nbawiki/ui/main/team/TeamFragment.kt
+++ app/src/main/java/com/example/nbawiki/ui/main/team/TeamFragment.kt
@@ -25,11 +25,16 @@ class TeamFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val teamName : String = arguments?.let {
-           TeamFragmentArgs.fromBundle(it).teamName
+        val teamName: String = arguments?.let {
+            TeamFragmentArgs.fromBundle(it).teamName
         } ?: ""
 
-        val binding = DataBindingUtil.inflate<TeamFragmentBinding>(inflater, R.layout.team_fragment, container, false)
+        val binding = DataBindingUtil.inflate<TeamFragmentBinding>(
+            inflater,
+            R.layout.team_fragment,
+            container,
+            false
+        )
 
         val viewModel = ViewModelProviders.of(this).get(TeamViewModel::class.java)
         viewModel.initializeTeamData(teamName)
@@ -45,20 +50,23 @@ class TeamFragment : Fragment() {
         viewPager = view.findViewById(R.id.pager)
         viewPager.adapter = demoCollectionPagerAdapter
 
-        val tabLayout : TabLayout = view.findViewById(R.id.tab_layout)
+        val tabLayout: TabLayout = view.findViewById(R.id.tab_layout)
         tabLayout.setupWithViewPager(viewPager)
     }
 }
 
 class DemoCollectionPagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
 
-    override fun getCount(): Int  = 2
+    override fun getCount(): Int = 2
 
-    override fun getItem(i: Int): Fragment {
-        val fragment = DemoObjectFragment()
+    override fun getItem(tabNumber: Int): Fragment {
+        val fragment = when (tabNumber) {
+            0 -> NewsListFragment()
+            else -> PlayerListFragment()
+        }
         fragment.arguments = Bundle().apply {
             // Our object is just an integer :-P
-            putInt(ARG_OBJECT, i + 1)
+            putInt(ARG_OBJECT, tabNumber + 1)
         }
         return fragment
     }
@@ -70,26 +78,42 @@ class DemoCollectionPagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapte
 
 private const val ARG_OBJECT = "object"
 
-// Instances of this class are fragments representing a single
-// object in our collection.
-class DemoObjectFragment : Fragment() {
+class NewsListFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         return inflater.inflate(R.layout.fragment_collection_object, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//
-//        val textView = view.findViewById<TextView>(R.id.tab_text)
-//        textView.text = "erwterghrhdfghdfghdfghdgfhdgfhdfgh"
 
         arguments?.takeIf { it.containsKey(ARG_OBJECT) }?.apply {
             val textView = view.findViewById<TextView>(R.id.tab_text)
-//                      val textView: TextView = view.findViewById(android.R.id.text1)
             textView.text = getInt(ARG_OBJECT).toString()
         }
+
+    }
+}
+
+class PlayerListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+//        arguments?.takeIf { it.containsKey(ARG_OBJECT) }?.apply {
+//            val textView = view.findViewById<TextView>(R.id.tab_text)
+//            textView.text = getInt(ARG_OBJECT).toString()
+//        }
 
     }
 }
