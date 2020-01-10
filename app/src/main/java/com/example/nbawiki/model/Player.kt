b9c477946c3489955103522816d301215840c466
Augustas dirzys <spekulents@gmail.com>
2020-01-10 17:29:21 +0200

    Add age weight and height binding for player profile

--- app/src/main/java/com/example/nbawiki/model/Player.kt
+++ app/src/main/java/com/example/nbawiki/model/Player.kt
@@ -6,7 +6,7 @@ data class Player (
     val id : Int = 0,
     val name : String = "defaulName",
     val sureName : String = "defaulSureName",
-    val age : Int = (20..50).random(),
+    val age : String = (20..50).random().toString(),
     val height : Int = (160..250).random(),
     val weight : Int = (80..150).random(),
     val description : String = UUID.randomUUID().toString()
