be570ee0e5c1ca50c09147af7b63f5b96661adf8
Augustas dirzys <spekulents@gmail.com>
2020-01-10 15:40:20 +0200

    Add player info initial xml

--- app/src/main/java/com/example/nbawiki/model/Player.kt
+++ app/src/main/java/com/example/nbawiki/model/Player.kt
@@ -3,5 +3,8 @@ package com.example.nbawiki.model
 data class Player (
     val id : Int = 0,
     val name : String = "defaulName",
-    val sureName : String = "defaulSureName"
+    val sureName : String = "defaulSureName",
+    val age : Int = (20..50).random(),
+    val height : Int = (160..250).random(),
+    val weight : Int = (80..150).random()
 )
