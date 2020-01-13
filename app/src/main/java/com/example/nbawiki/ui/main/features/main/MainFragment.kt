06382e919bb9cb2b497cb16296ad517468cddd07
Augustas dirzys <spekulents@gmail.com>
2020-01-13 18:19:29 +0200

    Add basic data binding to main fragment

--- app/src/main/java/com/example/nbawiki/ui/main/features/main/MainFragment.kt
+++ app/src/main/java/com/example/nbawiki/ui/main/features/main/MainFragment.kt
@@ -1,36 +1,42 @@
 package com.example.nbawiki.ui.main.features.main
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.nbawiki.R
-import com.example.nbawiki.model.Team
+import com.example.nbawiki.databinding.MainFragmentBinding
 import com.example.nbawiki.ui.main.features.main.recycleview.OnItemClickListener
 import com.example.nbawiki.ui.main.features.main.recycleview.TeamListAdapter
-import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainFragment : Fragment(), OnItemClickListener {
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var binding : MainFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        binding = DataBindingUtil.inflate<MainFragmentBinding>(
+            inflater,
+            R.layout.main_fragment,
+            container,
+            false
+        )
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         val nbaTeams = viewModel.teams.value ?: emptyList()
-        team_recycle_view.apply {
+        binding.teamRecycleView.apply {
             layoutManager = LinearLayoutManager(activity)
             adapter = TeamListAdapter(nbaTeams, this@MainFragment)
         }
--- app/src/main/java/com/example/nbawiki/ui/main/features/main/recycleview/TeamViewHolder.kt
+++ app/src/main/java/com/example/nbawiki/ui/main/features/main/recycleview/TeamViewHolder.kt
@@ -1,13 +1,10 @@
 package com.example.nbawiki.ui.main.features.main.recycleview
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.example.nbawiki.R
 import com.example.nbawiki.model.Team
 import com.squareup.picasso.Picasso
