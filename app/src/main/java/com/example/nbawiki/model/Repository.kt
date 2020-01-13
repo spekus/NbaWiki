13037ce57a4e9f1a8fa1b66751dc156ef84d79ac
Augustas dirzys <spekulents@gmail.com>
2020-01-13 13:46:58 +0200

    Decouple data creation logic

--- app/src/main/java/com/example/nbawiki/model/Repository.kt
+++ app/src/main/java/com/example/nbawiki/model/Repository.kt
@@ -7,9 +7,9 @@ import java.util.*
 
 object Repository {
 
-    private var _teams : MutableLiveData<List<Team>> = MutableLiveData(createTeams())
+    private var _teams: MutableLiveData<List<Team>> = MutableLiveData(createTeams())
 
-    val NbaTeams : LiveData<List<Team>>
+    val NbaTeams: LiveData<List<Team>>
         get() = _teams
 
     fun getTeams(): LiveData<List<Team>> {
@@ -17,32 +17,59 @@ object Repository {
     }
 
     fun getTheTeam(teamName: String): LiveData<Team> {
-        val theTeam : Team? = _teams.value?.first {
+        val theTeam: Team? = _teams.value?.first {
             it.teamName.equals(teamName)
         }
         return MutableLiveData<Team>(theTeam)
     }
 
-    fun getThePlayer(playerId: Int): LiveData<Player>{
-        return MutableLiveData<Player>(_teams.value!!.find { it.teamMembers.any { it.id == playerId} }!!.teamMembers.find { it.id == playerId })
+    fun getThePlayer(playerId: Int): LiveData<Player> {
+        return MutableLiveData<Player>(_teams.value!!.find { it.teamMembers.any { it.id == playerId } }!!.teamMembers.find { it.id == playerId })
     }
 
-    private fun createTeams() : List<Team>{
-        return        listOf( Team(teamName = "Raising ARIZONA",
-            teamDescription = UUID.randomUUID().toString(),
-            news = listOf(News("Raising Arizona","Vampire's Kiss"),
-                News("Raising Arizona2","Vampire's Kiss2")),
-            teamMembers = listOf(Player(1,"James"), Player(2,"Augustas"))
-        ),
-        Team("Vampire's Kiss", UUID.randomUUID().toString()),
-        Team("Con Air", UUID.randomUUID().toString()),
-        Team("Gone in 60 Seconds", UUID.randomUUID().toString()),
-        createRandomTeam())
+    private fun createTeams(): List<Team> {
+        return listOf(
+            Team(
+                teamName = "Raising ARIZONA",
+                teamDescription = UUID.randomUUID().toString(),
+                news = listOf(
+                    News("Raising Arizona", "Vampire's Kiss"),
+                    News("Raising Arizona2", "Vampire's Kiss2")
+                ),
+                teamMembers = listOf(Player(1, "James"), Player(2, "Augustas"))
+            ),
+            createRandomTeam(),
+            createRandomTeam(),
+            createRandomTeam(),
+            createRandomTeam()
+        )
     }
 
-    private fun createRandomTeam() : Team {
-        var faker : Faker = Faker()
-        return Team(faker.name().fullName(), faker.gameOfThrones().quote())
+    private fun createRandomTeam(): Team {
+        val faker = Faker()
+        return Team(
+            teamName = faker.name().fullName(),
+            teamDescription = faker.gameOfThrones().quote(),
+            news = createNews()
+        )
+    }
+
+    private fun createNews(): List<News> {
+        val faker = Faker()
+        return listOf(
+            News(
+                team =  faker.name().firstName(),
+                ennemyTeam = faker.name().firstName()
+            ),
+            News(
+                team =  faker.name().firstName(),
+                ennemyTeam = faker.name().firstName()
+            ),
+            News(
+                team =  faker.name().firstName(),
+                ennemyTeam = faker.name().firstName()
+            )
+        )
     }
 
 }
