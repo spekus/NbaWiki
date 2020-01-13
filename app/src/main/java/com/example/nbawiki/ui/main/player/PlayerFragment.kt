1742486bae4f519f58e590876a05dc691463876d
Augustas dirzys <spekulents@gmail.com>
2020-01-13 11:53:32 +0200

    Add basic navigation back from one fragment to another

--- app/src/main/java/com/example/nbawiki/ui/main/player/PlayerFragment.kt
+++ app/src/main/java/com/example/nbawiki/ui/main/player/PlayerFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 
 import com.example.nbawiki.R
 import com.example.nbawiki.databinding.PlayerFragmentBinding
@@ -41,13 +42,16 @@ class PlayerFragment : Fragment() {
 
         binding.player = viewModel.player.value
 
+        binding.playerBackButton.setOnClickListener {
+            findNavController().navigateUp()
+        }
+
         return binding.root
 
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-
 
     }
 
