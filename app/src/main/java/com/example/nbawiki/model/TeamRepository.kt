3958215e26d736200d4e119a9aa6ffe5526f57af
Augustas dirzys <spekulents@gmail.com>
2020-01-16 10:50:08 +0200

    Remove unused code

--- app/src/main/java/com/example/nbawiki/model/TeamRepository.kt
+++ app/src/main/java/com/example/nbawiki/model/TeamRepository.kt
@@ -9,9 +9,7 @@ import com.example.nbawiki.ui.main.util.api.ApiService
 import kotlinx.coroutines.*
 import timber.log.Timber
 
-class TeamRepository(private val nbaApiService : ApiService) : Repository{
-
-//    private var _teams: MutableLiveData<List<Team>> = MutableLiveData(createTeams())
+class TeamRepository(private val nbaApiService: ApiService) : Repository {
     private var _teams: MutableLiveData<List<Team>> = MutableLiveData()
 
     private var _theTeam: MutableLiveData<Team> = MutableLiveData()
@@ -22,72 +20,37 @@ class TeamRepository(private val nbaApiService : ApiService) : Repository{
     override val selectedTeam: LiveData<Team>
         get() = _theTeam
 
-
     private val parentJob = Job()
     private val coroutineScope = CoroutineScope(Dispatchers.Main + parentJob)
 
 
-
-    override fun updateTeams(){
-         coroutineScope.launch(Dispatchers.Main) {
-             val teamsDTO =  nbaApiService.getAllTeams()
-//            val teamas = withContext(Dispatchers.Default) {nbaApiService.getAllteams() }
-            Timber.e("team count - ${teamsDTO.count()}")
-
-            val teams :List<Team> =  teamsDTO.map {
-                    it.asPresentationModel()
-             }
-             _teams.postValue(teams)
-
-//             val id : Int ,
-//             val teamName : String = "DefaultName",
-//             val teamDescription : String = "DefaultDescription",
-//             val teamIcon : String = "iconLink",
-//             val teamMembers : List<com.example.nbawiki.model.dto.Player> = emptyList(),
-//             val news : List<News> = emptyList(),
-//             val imageUrl : String = "https://i.picsum.photos/id/$id/200/200.jpg"
-
-
-            //here live data should be updated
-         }
-//        Log.e("TeamRepository", teamsss.toString())
-//        return nbaTeams
+    override fun updateTeams() {
+        coroutineScope.launch(Dispatchers.Main) {
+            val teamsDTO = nbaApiService.getAllTeams()
+            val teams: List<Team> = teamsDTO.map {
+                it.asPresentationModel()
+            }
+            _teams.postValue(teams)
+        }
     }
 
     override fun updateTheTeam(id: Int) {
-        // BAD THIS IS CALLED ALL THE TIME
         coroutineScope.launch(Dispatchers.Main) {
             val news = nbaApiService.getNews(id.toString()).map { it.asPresentationModel() }
-
-//            _theTeam.postValue(nbaApiService.getATeams(id.toString()))
-
             var theTeam: Team? = _teams.value?.first {
                 it.id == id
             }
 
             theTeam!!.news = news
-
             _theTeam.postValue(theTeam)
 
             val player = nbaApiService.getPlayers(theTeam.teamName).map { it.asPresentationModel() }
             theTeam.teamMembers = player
             _theTeam.postValue(theTeam)
         }
-
-        val theTeam: Team? = _teams.value?.first {
-            it.id == id
-        }
-
-//        return MutableLiveData<Team>(theTeam)
     }
 
-    override fun updateThePlayer(id: Int) : MutableLiveData<Player> {
-//        coroutineScope.launch(Dispatchers.Main) {
-////            val value = nbaApiService.getPlayers("")
-////            value.get(0)
-//        }
-
-
+    override fun updateThePlayer(id: Int): MutableLiveData<Player> {
         return MutableLiveData<Player>(_teams.value!!.find { it.teamMembers.any { it.id == id } }!!.teamMembers.find { it.id == id })
     }
 }
