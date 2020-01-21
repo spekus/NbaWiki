f73be427247f6b235d7b90e4c1bb78b18591fb89
Augustas dirzys <spekulents@gmail.com>
2020-01-21 17:20:07 +0200

    Data is updated only when the time comes

--- app/src/main/java/com/example/nbawiki/model/UpdateTimes.kt
+++
@@ -1,4 +0,0 @@
-package com.example.nbawiki.model
-
-class UpdateTimes {
-}
--- app/src/main/java/com/example/nbawiki/network/network/TeamRepository.kt
+++ app/src/main/java/com/example/nbawiki/network/network/TeamRepository.kt
@@ -10,6 +10,9 @@ import com.example.nbawiki.model.presentation.Team
 import com.example.nbawiki.network.retrofit.WebService
 import com.example.nbawiki.ui.main.util.Event
 import com.example.nbawiki.ui.main.util.UpdateTime
+import com.github.guilhe.sharedprefsutils.ktx.get
+import com.github.guilhe.sharedprefsutils.ktx.put
+import timber.log.Timber
 import java.util.*
 
 
@@ -54,7 +57,7 @@ class TeamRepository(
     }
 
     override suspend fun refreshTeams() {
-        when (isItTimeToUpdateTeams()) {
+        when (isItTimeToUpdate(TEAM_PREF_KEY, UpdateTime.TEAM.timeBeforeUpdate)) {
             true -> {
                 updateTeamsViaApi()
             }
@@ -73,8 +76,12 @@ class TeamRepository(
         //load old data
         _theTeam.postValue(dataBase.getTheTeam(teamID))
         //refresh
-        refreshTeamNews(teamID)
-        refreshTeamPlayer(teamID)
+        if (isItTimeToUpdate(NEWS_PREF_KEY + teamID, UpdateTime.EVENT.timeBeforeUpdate)){
+            refreshTeamNews(teamID)
+        }
+        if (isItTimeToUpdate(PLAYER_PREF_KEY + teamID, UpdateTime.PLATER.timeBeforeUpdate)) {
+            refreshTeamPlayer(teamID)
+        }
         //update with new data
         _theTeam.postValue(dataBase.getTheTeam(teamID))
     }
@@ -90,8 +97,7 @@ class TeamRepository(
             players.forEach {
                 dataBase.putPlayer(it, teamID)
             }
-            val currentTime = Date(System.currentTimeMillis()).time
-            sharedPref.edit().putLong(PLAYER_TIME, currentTime).apply()
+            updateTimePreferences(PLAYER_PREF_KEY, teamID)
         } else {
             _didApiCallFail.postValue(Event(true))
         }
@@ -108,12 +114,15 @@ class TeamRepository(
             news.forEach {
                 dataBase.putNews(it, teamId)
             }
-            val currentTime = Date(System.currentTimeMillis()).time
-            sharedPref.edit().putLong(NEWS_TIME, currentTime).apply()
+
+            updateTimePreferences(NEWS_PREF_KEY, teamId)
+
+
         } else {
             _didApiCallFail.postValue(Event(true))
         }
     }
+
 
     private fun getSelectedTeam(teamId: Int): Team? {
         return _teams.value?.first {
@@ -136,7 +145,7 @@ class TeamRepository(
             _teams.postValue(dataBase.getAllTeams())
 
             val currentTime = Date(System.currentTimeMillis()).time
-            sharedPref.edit().putLong(TEAM_TIME, currentTime).apply()
+            sharedPref.put(TEAM_PREF_KEY, currentTime)
 
         } else {
             _didApiCallFail.postValue(Event(true))
@@ -144,10 +153,19 @@ class TeamRepository(
     }
 
 
-    private fun isItTimeToUpdateTeams(): Boolean {
-        val lastUpdate = sharedPref.getLong(TEAM_TIME, 0L)
+    private fun isItTimeToUpdate(sharePrefId : String, timeBeforeUpdate : Long ): Boolean {
+        val lastUpdate = sharedPref.get(sharePrefId, Long::class.java, 1)
+        if(lastUpdate == 1L){
+            Timber.e("could not get shared pref for key $sharePrefId")
+        }
         val timePassed = System.currentTimeMillis() - lastUpdate
-        return timePassed > UpdateTime.TEAM.timeBeforeUpdate
+        return timePassed > timeBeforeUpdate
+    }
+
+
+    private fun updateTimePreferences(key: String, teamId: Int) {
+        val currentTime = Date(System.currentTimeMillis()).time
+        sharedPref.put(key + teamId, currentTime)
     }
 
 }
@@ -156,6 +174,6 @@ class TeamRepository(
 const val LEAGUE_KEY = "4387"
 const val PRIVATE_MODE = 0
 const val PREF_NAME = "mindorks-welcome"
-const val TEAM_TIME = "team_update_time"
-const val PLAYER_TIME = "player_update_time"
-const val NEWS_TIME = "news_update_time"
+const val TEAM_PREF_KEY = "team_update_time"
+const val PLAYER_PREF_KEY = "player_update_time"
+const val NEWS_PREF_KEY = "news_update_time"
